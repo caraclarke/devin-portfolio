c8400aadc37f156be69a943682257c64adf40e67
caraclarke <cara.clarke8@gmail.com>
2015-12-09 22:04:33 -0500

    height fix navbar

--- js/hamburger_menu.js
+++ js/hamburger_menu.js
@@ -2,6 +2,7 @@ $(function(){
 
   $('.navbar-toggle').on('click',function(e){
       $('.container-fluid').toggleClass('open');
+      $('nav div.container').toggleClass('nav-height');
   });
 
 });
