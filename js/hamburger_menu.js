117b05c6619b2156b5addb97eebafbb021d88feb
caraclarke <cara.clarke8@gmail.com>
2015-12-10 22:19:48 -0500

    Lightbox mobile nav working

--- js/hamburger_menu.js
+++ js/hamburger_menu.js
@@ -1,11 +1,8 @@
 $(function(){
 
-  // var windowWidth = $(window).width();
-  //   if(windowWidth > 980){
-  //     $('#collapsed').on('click',function(e){
-  //       $('.overlay').toggleClass('open');
-  //       $('nav div.container').toggleClass('nav-height');
-  //     });
-  //   }
+$('.navbar-toggle').on('click',function(e){
+  $('.container-fluid').toggleClass('open');
+  $('nav div.container').toggleClass('nav-height');
+});
 
-};
+}());
