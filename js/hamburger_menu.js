b6b714187866a84b47208f5d15fabe4d61c21b13
caraclarke <cara.clarke8@gmail.com>
2015-12-10 20:52:40 -0500

    Rough desktop communit slideshow

--- js/hamburger_menu.js
+++ js/hamburger_menu.js
@@ -1,8 +1,11 @@
 $(function(){
 
-  $('.navbar-toggle').on('click',function(e){
-      $('.overlay').toggleClass('open');
-      $('nav div.container').toggleClass('nav-height');
-  });
+  // var windowWidth = $(window).width();
+  //   if(windowWidth > 980){
+  //     $('#collapsed').on('click',function(e){
+  //       $('.overlay').toggleClass('open');
+  //       $('nav div.container').toggleClass('nav-height');
+  //     });
+  //   }
 
-});
+};
---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('.slideshow-section').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        dots: true,
+        speed: 300,
+        centerMode: true,
+        arrows: true,
+        cssEase: 'ease',
+        initialSlide: 2,
+        respondTo: 'min',
+        swipeToSlide: true,
+        variableWidth: true,
+    });
+});
