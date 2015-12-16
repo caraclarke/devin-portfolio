1c2b652101010f878847dd4ca50589feb5648677
caraclarke <cara.clarke8@gmail.com>
2015-12-15 20:21:29 -0500

    Img end after load js

--- js/main.js
+++ js/main.js
@@ -14,4 +14,29 @@ $(document).ready(function() {
         swipeToSlide: true,
         variableWidth: true
     });
+
+    function stopAnimation() {
+        setTimeout(function() {
+        $('figure').css("-webkit-animation", "none");
+        $('figure').css("-moz-animation", "none");
+        $('figure').css("-ms-animation", "none");
+        $('figure').css("animation", "none");
+        $('figure').css("display", "none");
+        }, 5000);
+        setTimeout(function() {
+            $('#picture-anim').css("display", "none");
+        }, 7000);
+    }
+
+    stopAnimation();
+
+    $("#picture-anim").click(function() {
+        var el = $(this),
+        newone = el.clone(true);
+
+        el.before(newone);
+
+        $("." + el.attr("class") + ":last").remove();
+    });
+
 });
