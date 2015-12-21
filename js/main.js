7987288b672cb2e35d31f5a3c10b04085c1d50a5
caraclarke <cara.clarke8@gmail.com>
2015-12-20 19:35:25 -0500

    Broken but right parallax movement

--- js/main.js
+++ js/main.js
@@ -1,54 +1,55 @@
 $(document).ready(function() {
 
-    $('#fullpage').fullpage({
-        anchors: ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen'],
-        menu: '#myNavbar',
-        navigation: true,
-        navigationPosition: 'right'
-    });
+$('.fotorama').fotorama({
+    width: 600,
+    maxwidth: '100%',
+    ratio: 16/9,
+    allowfullscreen: true,
+    nav: 'thumbs',
+    thumbborderwidth: '50%',
+    startindex: 3
+});
 
-    // $(".main").onepage_scroll({
-    //      sectionContainer: "section",
-    //      easing: "ease",
-    //      animationTime: 1000,
-    //      pagination: true,
-    //      updateURL: false,
-    //      loop: false,
-    //      keyboard: true,
-    //      responsiveFallback: false,
-    //      direction: "vertical"
-    //  });
+(function() {
+    $('#myNavbar').addClass('hidden');
+    $('#navbar-eye').addClass('hidden');
+    setTimeout(function() {
+        $('#intro-video').css('display', 'none');
+        $('#myNavbar').removeClass('hidden');
+        $('#navbar-eye').removeClass('hidden');
+    }, 4100);
+}());
 
-    $('.fotorama').fotorama({
-        width: 600,
-        maxwidth: '100%',
-        ratio: 16/9,
-        allowfullscreen: true,
-        nav: 'thumbs',
-        thumbborderwidth: '50%',
-        startindex: 3
-    });
+$("#navbar-eye-img").on('click', function() {
+    $('#intro-video').css('display','block');
+    var video = $('#intro-video')[0];
+    if (video.paused) {
+        video.play();
+        setTimeout(function() {$('#intro-video').css('display', 'none')}, 4000);
+    }
+    else {
+        video.pause();
+    }
+    return false;
+});
 
-    (function() {
-        $('#myNavbar').addClass('hidden');
-        $('#navbar-eye').addClass('hidden');
-        setTimeout(function() {
-            $('#intro-video').css('display', 'none');
-            $('#myNavbar').removeClass('hidden');
-            $('#navbar-eye').removeClass('hidden');
-        }, 4100);
-    }());
+(function() {
+    //actualBlock is the section we are animating
+var offset = $(window).scrollTop() - actualBlock.offset().top,
+windowHeight = $(window).height();
 
-    $("#navbar-eye-img").on('click', function() {
-        $('#intro-video').css('display','block');
-        var video = $('#intro-video')[0];
-        if (video.paused) {
-            video.play();
-            setTimeout(function() {$('#intro-video').css('display', 'none')}, 4000);
-        }
-        else {
-            video.pause();
-        }
-        return false;
-    });
+if( offset >= -windowHeight && offset <= 0 ) {
+    // section entering the viewport
+    translateY = (-offset)*100/windowHeight;
+    scale = 1;
+    opacity = 1;
+  } else if( offset > 0 && offset <= windowHeight ) {
+    //section leaving the viewport
+    scale = (1 - ( offset * 0.3/windowHeight));
+    opacity = ( 1 - ( offset/windowHeight) );
+    translateY = 0;
+    boxShadowBlur = 40*(offset/windowHeight);
+  }
+}());
+
 });
